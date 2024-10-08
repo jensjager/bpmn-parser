7947edb6dbc4cb460b31e476d1daa4b9d3662fec
Morris Tann <morris.tann@gmail.com>
2024-10-08 19:20:27 +0300

    Add Vertex struct to represent graph vertices with ID and layer

--- src/layout/node.rs
+++ src/layout/node.rs
@@ -1,1 +1,10 @@
+pub struct node {
+    pub id: usize,
+    pub layer: Option<i32>
+}
 
+impl node {
+    pub fn new(id: usize, layer: Option<i32>) -> node {
+        node { id, layer: None}
+    }
+}
