843dcd0e5e34077108de68a07e3841e08337e736
luckasw <albert.wihler@gmail.com>
2024-10-31 02:28:14 +0200

    Fix: add y_offset to center smaller events on y axis

--- src/layout/node_positioning.rs
+++ src/layout/node_positioning.rs
@@ -1,5 +1,6 @@
-use crate::common::graph::Graph;
+use crate::common::{bpmn_event, graph::Graph};
 use std::collections::HashMap;
+use crate::common::bpmn_event::BpmnEvent;
 
 // Määrame sõlmede suurused ja vahed
 const NODE_HEIGHT: f64 = 50.0;        // Sõlme kõrgus
@@ -10,28 +11,40 @@ const LAYER_WIDTH: f64 = NODE_WIDTH + 50.0; // X-koordinaatide vahe (kihiti)
 /// Määrab sõlmede X ja Y koordinaadid kihtide ja järjestuse põhjal.
 pub fn assign_xy_to_nodes(graph: &mut Graph, layers: &Vec<(usize, i32)>) {
     let layer_width = LAYER_WIDTH; // Kasutame määratud kihivahet
-    let node_spacing = NODE_HEIGHT + MIN_NODE_SPACING;  // Y-koordinaatide vahe sõlmede vahel (kihis)
-
+    
     // Loome kaardi, et rühmitada sõlmed kihtide järgi
     let mut layer_map: HashMap<i32, Vec<usize>> = HashMap::new();
     for (node_id, layer) in layers {
         layer_map.entry(*layer).or_insert(vec![]).push(*node_id);
     }
-
+    
     // Määrame igale kihile ja sõlmele koordinaadid
     for (layer, nodes) in layer_map.iter() {
         let x_start = 100.0; // X-koordinaadi alguspunkt
         let x = x_start + (*layer as f64 * layer_width); // X-koordinaat määratakse kihi numbri ja alguspunkti järgi
         //let x = *layer as f64 * layer_width; // X-koordinaat määratakse kihi numbri järgi
-        let mut y_position = 100.0;
-
+        let mut y_position = 200.0;
+        
         // Sõlmede joondamine kihis
         for node_id in nodes {
-            let y = y_position;
+            let node = graph.nodes.iter().find(|n| n.id == *node_id);
+            let node_event = &node.unwrap().event.as_ref().unwrap();
+            let (_, node_size_y) = bpmn_event::get_node_size(node_event);
+            let mut node_spacing =  node_size_y as f64 + MIN_NODE_SPACING;  // Y-koordinaatide vahe sõlmede vahel (kihis)
+            if node_spacing < 100.0 {
+                node_spacing += 100.0 - node_spacing;
+                println!("Väiksem kui 100: {}", node_spacing);
+            }
+            let y_offset = if node_size_y < 80 {
+                (80 - node_size_y) as f64 / 2.0
+            } else {
+                0.0
+            };
+            let y = y_position + y_offset;
             y_position += node_spacing;
 
             if let Some(node) = graph.nodes.iter_mut().find(|n| n.id == *node_id) {
-                // println!("Määran sõlmele {} koordinaadid: x = {}, y = {}", node_id, x, y);
+                println!("Määran sõlmele {} koordinaadid: x = {}, y = {}", node_id, x, y);
                 node.set_position(x, y);
             } else {
                 println!("Sõlme {} ei leitud graafis", node_id);
